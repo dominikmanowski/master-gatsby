67b2f974389ac665b01d06be4c899fba8655a9a9
Dominik Manowski <manowski@protonmail.com>
2020-11-21 22:12:54 +0100

    gatsby | add global styles and put them in layout file

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Nav from './Nav'
+import GlobalStyles from '../styles/GlobalStyles'
 
 const Layout = ({ children }) => (
   <div>
+    <GlobalStyles />
     <Nav />
     {children}
   </div>
