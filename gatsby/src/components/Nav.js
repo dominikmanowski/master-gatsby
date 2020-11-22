571a3498a846443f63ccb59cbe4c178bf33941c4
Dominik Manowski <manowski@protonmail.com>
2020-11-22 09:13:33 +0100

    gatsby | add styles for nav

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -1,8 +1,45 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
+
+const StyledNav = styled.nav`
+  margin-bottom: 3rem;
+  ul {
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    list-style: none;
+    display: grid;
+    grid-template-columns: 1fr 1fr auto 1fr 1fr;
+  }
+  li {
+    --rotate: -2deg;
+    transform: rotate(var(--rotate));
+    order: 1;
+    &:nth-child(1) {
+      --rotate: 1deg;
+    }
+    &:nth-child(2) {
+      --rotate: -2.5deg;
+    }
+    &:nth-child(4) {
+      --rotate: 2.5deg;
+    }
+    &:hover {
+      --rotate: 3deg;
+    }
+  }
+  a {
+    font-size: 3rem;
+    text-decoration: none;
+    &:hover {
+      color: var(--red);
+    }
+  }
+`
 
 const Nav = () => (
-  <nav>
+  <StyledNav>
     <ul>
       <li>
         <Link to="/">Hot Now!</Link>
@@ -20,7 +57,7 @@ const Nav = () => (
         <Link to="/order">Order</Link>
       </li>
     </ul>
-  </nav>
+  </StyledNav>
 )
 
 export default Nav
