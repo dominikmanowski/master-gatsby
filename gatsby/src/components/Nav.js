97ddf779b586dab707d37754833849886aa668c9
Dominik Manowski <manowski@protonmail.com>
2020-11-22 09:22:02 +0100

    gatsby | add logo component and put in in nav

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import styled from 'styled-components'
+import Logo from './Logo'
 
 const StyledNav = styled.nav`
-  margin-bottom: 3rem;
+  .logo {
+    transform: translateY(-25%);
+  }
   ul {
     margin: 0;
     padding: 0;
@@ -11,6 +14,8 @@ const StyledNav = styled.nav`
     list-style: none;
     display: grid;
     grid-template-columns: 1fr 1fr auto 1fr 1fr;
+    grid-gap: 2rem;
+    align-items: center;
   }
   li {
     --rotate: -2deg;
@@ -45,10 +50,12 @@ const Nav = () => (
         <Link to="/">Hot Now!</Link>
       </li>
       <li>
-        <Link to="/pizzas">Menu</Link>
+        <Link to="/pizzas">Pizza Menu</Link>
       </li>
       <li>
-        <Link to="/">LOGO</Link>
+        <Link to="/">
+          <Logo />
+        </Link>
       </li>
       <li>
         <Link to="/slicemasters">Slice Masters</Link>
