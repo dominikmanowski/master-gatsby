66de75a2558953bb14fcf6600eb39897da6494b7
Dominik Manowski <manowski@protonmail.com>
2020-11-22 08:45:15 +0100

    gatsby | add typography styles and put them in layout

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from './Nav'
 import GlobalStyles from '../styles/GlobalStyles'
+import Typography from '../styles/Typography'
 
 const Layout = ({ children }) => (
   <div>
     <GlobalStyles />
+    <Typography />
     <Nav />
     {children}
   </div>
